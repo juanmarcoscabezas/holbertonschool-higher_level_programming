9ff5573dd6e3cec95770d96934fb2846c0cf696b
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-10 18:04:41 +0000

    Fixing task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if j > i:
+        if j > i and i < 8:
             print("{:d}{:d}".format(i, j), end=', ')
-else:
-    print("89")
+            continue
+        if j >= 8 and j > i:
+            print("{:d}{:d}".format(i, j))
