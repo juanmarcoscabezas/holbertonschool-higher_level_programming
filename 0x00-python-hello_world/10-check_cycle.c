3ac8ea866bf04590c9f72f65cc71d5e7b3a301c8
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-10 01:41:53 +0000

    Adding NULL exception

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,6 +11,9 @@ int check_cycle(listint_t *list)
 	listint_t *tortoise = list;
 	listint_t *hare = list;
 
+	if (!list)
+		return (0);
+
 	while (tortoise && hare)
 	{
 		tortoise = tortoise->next;
