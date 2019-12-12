55ba76220a5896fc3f43416d290d4c5e719e4acf
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-12 17:28:25 +0000

    0 arguments condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
-    if length == 1:
+    if length == 0:
+        print("{:d} arguments.".format(length))
+    elif length == 1:
         print("{:d} argument:".format(length))
     else:
         print("{:d} arguments:".format(length))
