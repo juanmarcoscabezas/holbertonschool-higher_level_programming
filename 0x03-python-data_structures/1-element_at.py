3c6a1d0e43b5468723b272e77f65fbde7fb351e8
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-17 17:23:29 +0000

    Working on task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for number in my_list:
-        print("{:d}".format(number))
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list) - 1:
+        return None
+    return my_list[idx]
