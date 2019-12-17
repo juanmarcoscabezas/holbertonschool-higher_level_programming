eaa1e9997499cde762bf524a1a8f0fd2450cbf7c
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-17 22:59:15 +0000

    Negative cases

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0 or not my_list:
         return None
-    max_val = 0
+    max_val = my_list[0]
     for item in my_list:
         if item > max_val:
             max_val = item
