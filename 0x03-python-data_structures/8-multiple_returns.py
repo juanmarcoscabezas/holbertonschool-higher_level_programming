901eb993a1a8d3762d43f0230a058be9170e0616
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-18 01:47:49 +0000

    Case when len is 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    res = len(sentence), sentence[0]
+    if len(sentence) > 0:
+        res = len(sentence), sentence[0]
+        return res
+    res = len(sentence), None
     return res
