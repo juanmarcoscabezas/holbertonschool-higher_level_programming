d4e3664a5dbaa5619e24a30ec1ecda3a208dd964
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-19 18:22:24 +0000

    Fixing pep8

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     set_new = set()
     for val in set_1:
-        if not val in set_2:
+        if not (val in set_2):
             set_new.add(val)
     for val in set_2:
-        if not val in set_1:
+        if not (val in set_1):
             set_new.add(val)
     return set_new
