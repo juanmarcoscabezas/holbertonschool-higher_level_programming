b24554339e8a813c8246c5961e76da7c8da7cd1a
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-19 18:21:42 +0000

    Working on task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    set_new = set()
+    for val in set_1:
+        if not val in set_2:
+            set_new.add(val)
+    for val in set_2:
+        if not val in set_1:
+            set_new.add(val)
+    return set_new
