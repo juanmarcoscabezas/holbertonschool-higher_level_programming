297f13bc9967b3b55e2e6286e2bb71077a37698e
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-19 16:51:12 +0000

    Working on task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    total = 0
+    for element in my_list:
+        if not (element in new_list):
+            new_list.append(element)
+            total += element
+    return total
