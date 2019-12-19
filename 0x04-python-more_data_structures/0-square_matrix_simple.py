592a6b63fc239ca36f444976e32a2d260a0b6bb2
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-19 17:17:19 +0000

    Fixing pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,7 @@
 def square_m(number):
         return (number ** 2)
 
+
 def square_matrix_simple(matrix=[]):
     new_matrix = [list(map(square_m, vector)) for vector in matrix]
     return (new_matrix)
