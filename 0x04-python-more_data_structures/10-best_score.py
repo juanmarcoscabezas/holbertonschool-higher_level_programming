2e34ceef42159f7184c07cf9f48fcafaee3da658
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-12-20 02:26:05 +0000

    Working on task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    best = ""
     score = 0
     has_change = False
     if a_dictionary:
-        for val in a_dictionary.values():
-            if val > 0:
-                score = val
+        for key in a_dictionary:
+            if a_dictionary[key] > score:
+                score = a_dictionary[key]
+                best = key
             has_change = True
-    if has_change:
-        return score
+    if best != "":
+        return best
     return None
