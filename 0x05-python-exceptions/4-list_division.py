dde1af1abd23e5e94426389a04764f0c374710f4
Juan Marcos Cabezas <1225@holbertonschool.com>
2020-01-08 21:23:48 +0000

    Working on task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,14 @@
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [0] * list_length
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        finally:
+            new_list[i] = res
+    return new_list
