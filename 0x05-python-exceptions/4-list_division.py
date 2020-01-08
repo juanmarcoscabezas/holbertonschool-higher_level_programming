71697b4d634a2a5e27c4e02eaeac472940ecedd6
Juan Marcos Cabezas <1225@holbertonschool.com>
2020-01-08 21:26:25 +0000

    now thw e zeros appear

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0] * list_length
     for i in range(list_length):
+        res = 0
         try:
             res = my_list_1[i] / my_list_2[i]
         except IndexError:
