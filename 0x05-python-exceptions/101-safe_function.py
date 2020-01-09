ba4e8b993e7caeabf51a1555cd050becc8a56198
juanmarcoscabezas <juanmarcoscabezas@gmail.com>
2020-01-09 03:17:36 +0000

    Woriing on task 102

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except BaseException as be:
+        sys.stderr.write("Exception: %s\n" % be)
+        res =  None
+    finally:
+        return res
