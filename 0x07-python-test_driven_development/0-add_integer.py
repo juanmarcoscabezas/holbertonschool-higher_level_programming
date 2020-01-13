32ee1e44d178b00daf4b5b621733b976673ae87e
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-13 18:40:27 +0000

    Fixing some typos

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,9 +15,9 @@ def add_integer(a, b=98):
     """
 
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be integer")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
