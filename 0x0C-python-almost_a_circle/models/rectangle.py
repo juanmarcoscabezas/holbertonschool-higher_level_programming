6c34f312db48add9ba0e7b48c33141494f4a9664
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-27 22:11:31 +0000

    Working on task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,7 +86,11 @@ class Rectangle(Base):
 
     def display(self):
         """prints the rectangle with #"""
+        for y in range(self.y):
+            print()
         for h in range(self.height):
+            for x in range(self.x):
+                    print(" ", end="")
             for w in range(self.width):
                 print("#", end="")
             print()
