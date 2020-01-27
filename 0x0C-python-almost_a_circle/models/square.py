20cc21a783071c56caf8623ed0be5342032033e3
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-27 23:13:19 +0000

    Working on task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,14 @@ class Square(Rectangle):
         """overrifing str method"""
         text = "[Square] ({:d}) {:d}/{:d} - {:d}"
         return text.format(self.id, self.x, self.y, self.width, self.height)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
