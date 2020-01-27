e5e688c17e61b0d5428ce386160f37c49850b6ad
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-27 23:50:32 +0000

    Working on task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -56,3 +56,7 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """parse to dictionary"""
+        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
