80314f1e5970d877c672597fe18a8f20ceb73c90
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-27 21:10:48 +0000

    Adding comments to getters and setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
-from models.base import Base
-
-
 """
 Rectangle class
 """
+
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -22,32 +21,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """width setter"""
         self.__width = value
 
     @property
     def height(self):
+        """height getter"""
         return self.__height
 
     @height.setter
-    def width(self, value):
-        self.__width = value
+    def height(self, value):
+        """height setter"""
+        self.__height = value
 
     @property
     def x(self):
+        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """x setter"""
         self.__x = value
 
     @property
     def y(self):
+        """y getter"""
         return self.__y
 
     @y.setter
     def width(self, value):
+        """y setter"""
         self.__y = value
