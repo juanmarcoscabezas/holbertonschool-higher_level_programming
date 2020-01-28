c8fcb02f0bba466601b63f23cdedcef3863c0b9f
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-01-28 03:46:30 +0000

    Adding cases to rectangle class

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -17,16 +17,39 @@ class ClassBaseTest(unittest.TestCase):
     def test_00(self):
         """test no id"""
         base = Base()
+        self.assertEqual(base.id, 2)
 
     def test_01(self):
         """test id number"""
         base = Base(1)
+        self.assertEqual(base.id, 1)
 
     def test_02(self):
         """test id string"""
         base = Base("Holberton")
+        self.assertEqual(base.id, "Holberton")
 
     def test_03(self):
-        """to_json_string"""
-        base = Base()
-        base.to_json_string([{'x': 10}])
+        """test id float"""
+        base = Base(1.4)
+        self.assertEqual(base.id, 1.4)
+
+    def test_04(self):
+        """test id negative"""
+        base = Base(-3)
+        self.assertEqual(base.id, -3)
+
+    def test_05(self):
+        """test id None"""
+        base = Base(None)
+        self.assertEqual(base.id, 8)
+
+    def test_06(self):
+        """test id dict"""
+        base = Base({'id': 7})
+        self.assertEqual(base.id, {'id': 7})
+
+    def test_04(self):
+        """test id list"""
+        base = Base([1,2,3])
+        self.assertEqual(base.id, [1,2,3])
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+"""
+Unittest class base
+"""
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class ClassRectangleTest(unittest.TestCase):
+
+    def setUp(self):
+        rec = Rectangle(1, 1)
+
+    def test_00(self):
+        """test success"""
+        r = Rectangle(1, 1)
+        self.assertEqual(r.id, 11)
+
+    def test_01(self):
+        """test sucess"""
+        r = Rectangle(1, 1)
+        self.assertEqual(r.id, 13)
+
+    def test_02(self):
+        """test height string"""
+        with self.assertRaises(TypeError): Rectangle(1, "Holberton")
+
+    def test_03(self):
+        """test height float"""
+        with self.assertRaises(TypeError): Rectangle(1, 1.4)
+
+    def test_04(self):
+        """test height negative"""
+        with self.assertRaises(ValueError): Rectangle(1, -3)
+
+    def test_05(self):
+        """test height None"""
+        with self.assertRaises(TypeError): Rectangle(1, None)
+
+    def test_06(self):
+        """test height dict"""
+        with self.assertRaises(TypeError): Rectangle(1, {'id': 7})
+
+    def test_04(self):
+        """test height list"""
+        with self.assertRaises(TypeError): Rectangle(1, [1,2,3])
