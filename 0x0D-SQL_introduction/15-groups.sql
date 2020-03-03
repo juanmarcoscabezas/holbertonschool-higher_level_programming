7e5da7b1a1d7b43bf78b2d407e7478f380d52a1a
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-03 02:19:31 +0000

    SRLY THE DESC?

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- This script the number of records with the same score in the table second_table
-SELECT score, count(*) AS "number" FROM second_table GROUP BY score ORDER BY "number";
+SELECT score, count(*) AS "number" FROM second_table GROUP BY score ORDER BY "number" DESC;
