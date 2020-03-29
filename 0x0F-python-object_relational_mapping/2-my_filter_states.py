c1b15aae829bf63230f58d344c5f5a7dde7cc1c3
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-29 13:12:28 +0000

    Working on task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
+        USER = sys.argv[1]
+        PASSWD = sys.argv[2]
+        DATABASE = sys.argv[3]
+
+        try:
+            db = MySQLdb.connect(host="localhost", port=3306,
+                                 user=USER, passwd=PASSWD, db=DATABASE)
+            cursor = db.cursor()
+            query = "SELECT * \
+                            FROM states \
+                            WHERE name = '{}' \
+                            ORDER BY id ASC".format(sys.argv[4])
+            cursor.execute(query)
+
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+        except Exception as ex:
+            print(ex)
