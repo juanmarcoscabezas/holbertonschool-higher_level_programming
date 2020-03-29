241905f2d8a2658f7c700a162bc5c13ff614ac9e
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-29 14:02:02 +0000

    Working on task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        USER = sys.argv[1]
+        PASSWD = sys.argv[2]
+        DATABASE = sys.argv[3]
+
+        try:
+            db = MySQLdb.connect(host="localhost", port=3306,
+                                 user=USER, passwd=PASSWD, db=DATABASE)
+            cursor = db.cursor()
+            query = "SELECT c.id, c.name, s.name \
+                     FROM cities AS c \
+                     RIGHT JOIN states as s \
+                     ON c.state_id = s.id \
+                     ORDER BY id ASC"
+            cursor.execute(query)
+
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+        except Exception as ex:
+            print(ex)
