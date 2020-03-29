9b076b44cde6505a208a489da62dfb6dcf006b19
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-29 01:40:58 +0000

    Adding id ASC

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
             db = MySQLdb.connect(host="localhost", port=3306,
                                  user=USER, passwd=PASSWD, db=DATABASE)
             cursor = db.cursor()
-            cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%'""")
+            cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                        ORDER BY id ASC""")
             rows = cursor.fetchall()
             for row in rows:
                 print(row)
