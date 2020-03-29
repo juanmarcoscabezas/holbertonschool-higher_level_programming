77d9218b6679f9c8db7af8c810bfb020e181753b
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-29 17:24:44 +0000

    Working on task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        USER = sys.argv[1]
+        PASSWD = sys.argv[2]
+        DATABASE = sys.argv[3]
+
+        DB_CON = 'mysql+mysqldb://{}:{}@localhost/{}'
+        engine = create_engine(DB_CON.format(USER, PASSWD, DATABASE),
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+
+        session = Session(engine)
+
+        new_state = State(name="Louisiana")
+        session.add(new_state)
+        session.commit()
+
+        instance = session.query(State).\
+            filter_by(name="Louisiana").first()
+
+        if instance is not None:
+            print('{}'.format(instance.id))
+        else:
+            print('Not inserted')
