bcb03458292f4bd70ac21c90f10f48463c1e7f1c
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-10 21:31:00 +0000

    refactor

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-if [ $# -gt 0 ]; then
-	curl -si --stderr - "$1" | grep "HTTP/1.0" | cut -b 10-12
-fi
+# displays only the status code of the response.
+curl -si -X GET --stderr - "$1" | grep "HTTP/1.0" | cut -b 10-12
