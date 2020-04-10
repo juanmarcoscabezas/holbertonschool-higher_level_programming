9604400d8128331ab9046b4deb2e9fd3dc10bd8d
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-10 22:14:15 +0000

    refactor

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,17 +5,15 @@
 
 
 def find_peak(list_of_integers):
-   """"Finds a peak in a list"""
-   if list_of_integers:
-      peak = list_of_integers[0]
+    """"Finds a peak in a list"""
+    if list_of_integers:
+        peak = list_of_integers[0]
 
-      for i, num in enumerate(list_of_integers):
-          if i > 0 and i < len(list_of_integers) - 1:
-              if list_of_integers[i - 1] < num and list_of_integers[i + 1] < num:
-                  peak = num
-                  continue
-          if num > peak:
-              peak = num
-      return peak
-   else:
-      return None
+        for i, num in enumerate(list_of_integers):
+            if i > 0 and i < len(list_of_integers) - 1:
+                if list_of_integers[i - 1] < num and list_of_integers[i + 1] < num and num > peak:
+                    peak = num
+                    break
+        return peak
+    else:
+        return None
