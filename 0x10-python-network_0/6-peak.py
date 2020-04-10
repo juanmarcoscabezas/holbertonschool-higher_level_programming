94697acdd0f6a33adf13677fd3cb15259c829b11
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-10 22:41:44 +0000

    Tryingwq dividing and conquer

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,13 +7,18 @@
 def find_peak(list_of_integers):
     """"Finds a peak in a list"""
     if list_of_integers:
-        peak = list_of_integers[0]
+        if len(list_of_integers) == 1:
+            return list_of_integers[0]
+        if list_of_integers[0] > list_of_integers[1]:
+            return list_of_integers[0]
+        if list_of_integers[-1] > list_of_integers[-2]:
+            return list_of_integers[-1]
 
-        for i, num in enumerate(list_of_integers):
-            if i > 0 and i < len(list_of_integers) - 1:
-                if list_of_integers[i - 1] < num and list_of_integers[i + 1] < num and num > peak:
-                    peak = num
-                    break
-        return peak
-    else:
-        return None
+        mid_val = len(list_of_integers) // 2
+        if list_of_integers[mid_val] >= list_of_integers[mid_val + 1] \
+                and list_of_integers[mid_val] >= list_of_integers[mid_val - 1]:
+            return list_of_integers[mid_val]
+        if list_of_integers[mid_val - 1] > list_of_integers[mid_val]:
+            return find_peak(list_of_integers[:mid_val])
+        if list_of_integers[mid_val + 1] > list_of_integers[mid_val]:
+            return find_peak(list_of_integers[mid_val + 1:])
