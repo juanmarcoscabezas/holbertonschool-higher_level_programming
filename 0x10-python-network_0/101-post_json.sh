c009b5bfb4128a3e92fb3eb6d3360e5fd61bc22d
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-10 21:52:53 +0000

    refactor

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays only the status code of the response.
-curl -s -H "Content-Type: application/json" -X POST "$1" -T "$2"
+curl -s -H "Content-Type: application/json" -X POST "$1" -d "@$2"
