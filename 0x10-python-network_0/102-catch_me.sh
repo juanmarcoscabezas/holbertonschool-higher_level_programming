56de990a97dcd5bb363198e2ec27b7ba31d90995
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-10 21:57:59 +0000

    Adding everything

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me
-curl -X PUT -H "Origin:HolbertonSchool" "$1"
+curl -X PUT -H "Origin:HolbertonSchool" -L -F "user_id=98" 0.0.0.0:5000/catch_me
