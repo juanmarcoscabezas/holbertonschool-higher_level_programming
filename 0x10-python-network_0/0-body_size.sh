d56cfbc1349de2d7143abdd9eb3f8fe5e07591ec
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-10 21:19:06 +0000

    Adding GET

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -si --stderr - "$1" | grep "Content-Length" | cut -d " " -f 2
+# DIsplays the size of the body in the response
+curl -si -X GET --stderr - "$1" | grep "Content-Length" | cut -d " " -f 2
