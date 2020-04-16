1390c1ddb230a8f0b9780f7e6f1adc70a40f7c5c
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-16 09:32:11 -0500

    Fixing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-
 """this script fetches https://intranet.hbtn.io/status
 """
+
+import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         body = response.read()
         print('Body response:')
-        print('    - type: {}'.format(type(body)))
-        print('    - content: {}'.format(body))
-        print('    - utf8 content: {}'.format(body.decode('utf8')))
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf8')))
