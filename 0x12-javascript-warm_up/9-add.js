f71f9a0c4e816b5df3d6fa996b93865c2619d280
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-05 23:22:23 +0000

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+Sscript that prints a square
+*/
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
+}
+
+add(a, b);
