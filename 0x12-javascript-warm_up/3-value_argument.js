058c404b969186bb3697ada035e73885172f92de
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-05 22:19:43 +0000

    Fixing requirements

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,9 +4,13 @@
 Sscript that prints a message depending
 of the number of arguments passed
 */
-const len = process.argv.length;
-if (len === 2) {
+let hasParam = false;
+process.argv.forEach((val, index) => {
+  if (index === 2) {
+    hasParam = true;
+    console.log(val);
+  }
+});
+if (!hasParam) {
   console.log('No argument');
-} else {
-  console.log(process.argv[2]);
 }
