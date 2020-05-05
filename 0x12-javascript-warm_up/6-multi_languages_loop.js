89eccfd5643299d05e50f5bfa3baa74b8c7f6aaf
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-05 23:06:57 +0000

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+Script that prints Javascript is amazing
+*/
+const lan = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < lan.length; i++) {
+  console.log(lan[i]);
+}
