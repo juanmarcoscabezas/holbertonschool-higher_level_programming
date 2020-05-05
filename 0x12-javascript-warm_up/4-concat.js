60ea6d6acbbbf8143c35d28438721289b0366648
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-05 22:23:31 +0000

    Task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/*
+Sscript that prints two arguments passed to it,
+in the following format: is
+*/
+const string = process.argv[2] + ' is ' + process.argv[3];
+console.log(string);
