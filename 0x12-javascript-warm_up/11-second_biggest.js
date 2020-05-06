2b4db39b31dcfdced532994bb0f9950f465d0205
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-06 00:15:07 +0000

    New mthod

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,27 @@ Sscript that searches the second biggest integer
 in the list of arguments
 */
 
-let numbers = process.argv;
-numbers.shift();
-numbers.shift();
-numbers = numbers.sort();
-if (numbers.length < 2) {
-  console.log(0);
-} else {
-  console.log(numbers[numbers.length - 2]);
+if (process.argv.length < 4) {
+  console.log(1);
+  process.exit();
 }
+
+let first = process.argv[2];
+let second = process.argv[3];
+
+if (first < second) {
+  const aux = first;
+  first = second;
+  second = aux;
+}
+
+const newArray = process.argv;
+newArray.shift();
+newArray.shift();
+newArray.forEach(val => {
+  if (val > first) {
+    second = first;
+    first = val;
+  }
+});
+console.log(second);
