cbb484279a6feb2abb714f6b9803639cbf5fd94e
juanmarcos <juanmarcoscabezas@gmail.com>
2020-05-11 12:51:34 -0500

    task 100

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const list = require('./100-data').list;
+
+const newList = list.map((element, index) => {
+  return element * index;
+});
+
+console.log(list);
+console.log(newList);
